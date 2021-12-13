0cbf4ba07ae805c521912fbeca86f2084f9f4d80
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2021-12-14 05:36:17 +0900

    形態素解析テスト

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,7 +1,19 @@
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
   if (msg.gobi) {
-    console.log(msg.gobi);
+    console.log(msg.gobi)
   }
 });
 
+import kuromoji from "kuromoji"
+const path = require("path")
 
+
+const text = "すもももももももものうち"
+kuromoji.builder({ dicPath: chrome.extension.getURL(path.join(__dirname, "/dict")) }).build((error, tokenizer) => {
+  if (error) {
+    console.log(error)
+  } else {
+    const tokens = tokenizer.tokenize(text)
+    console.log(tokens)
+  }
+})
