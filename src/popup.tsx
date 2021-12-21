264daaccaab68259df0e4e24e3a39105d0a79129
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2021-12-22 02:26:57 +0900

    語尾をlocal storageに保存と読み込み

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,23 +1,36 @@
-import React, { useEffect, useState } from "react";
-import ReactDOM from "react-dom";
+import React, { useEffect, useState } from "react"
+import ReactDOM from "react-dom"
 
 const Popup = () => {
+  const [str, setStr] = useState<string>("")
+
+  useEffect(() => {
+    chrome.storage.local.get(
+      {
+        gobi: "にゃ",
+      },
+      (value) => {
+        setStr(value.gobi);
+      }
+    );
+  }, []);
+
   const changeGobi = () => {
-    const gobi = (document.getElementById("gobi") as HTMLInputElement).value;
+    const gobi = (document.getElementById("gobi") as HTMLInputElement).value
+    chrome.storage.local.set({ "gobi": gobi }, function () { });
+
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      const tab = tabs[0];
+      const tab = tabs[0]
       if (tab.id) {
         chrome.tabs.sendMessage(
           tab.id,
           {
             gobi: gobi,
           }
-        );
+        )
       }
-    });
-  };
-
-  const [str, setStr] = useState("にゃ");
+    })
+  }
 
   return (
     <div>
@@ -26,17 +39,16 @@ const Popup = () => {
         id="gobi"
         type="text"
         defaultValue={str}
-        onChange={event => setStr(event.target.defaultValue)}
+        onChange={event => setStr(event.target.value)}
       />
       <button onClick={changeGobi}>語尾を変更</button>
     </div>
-  );
-};
-
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <Popup />
   </React.StrictMode>,
   document.getElementById("root")
-);
+)
